4d497da7230cbe8faa46aea82ad389471ec6ca6b
dolvin17 <huertas876@gmail.com>
2023-02-18 20:17:24 +0100

    chores: update tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+	  "./src/components/**/*.js",
+	  "./src/pages/**/*.js",
+	  "./src/pages/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
